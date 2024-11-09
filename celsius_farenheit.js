9ed16b83f51c6d8912abdc6993ac8c22147460ed
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-09 22:10:26 +0200

    add celsius_farenheit.js

---
+++ celsius_farenheit.js
@@ -0,0 +1,22 @@
+function cToF(celsius) {
+    if (!Number.isInteger(celsius)) {
+      return undefined;
+    }
+   
+    return (celsius * 9) / 5 + 32;
+  }
+   
+  function fToC(fahrenheit) {
+    if (!Number.isInteger(fahrenheit)) {
+      return undefined;
+    }
+   
+    return ((fahrenheit - 32) * 5) / 9;
+  }
+   
+  module.exports = {
+    cToF,
+    fToC,
+  };
+
+  console.log(cToF(5));
