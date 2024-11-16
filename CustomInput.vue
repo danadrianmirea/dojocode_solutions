a812ce3d1036686eee315ea0b36398f4b97224d6
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 03:41:44 +0200

    new file:   CustomInput.vue

---
+++ CustomInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Custom Input</h1>
+    <input type="text" :value="modelValue" @input="updateValue" />
+  </div>
+</template>
+
+<script setup>
+  import { defineProps, defineEmits } from "vue";
+
+  const props = defineProps({
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const emit = defineEmits(["update:modelValue"]);
+
+  const updateValue = event => {
+    emit("update:modelValue", event.target.value);
+  };
+</script>
