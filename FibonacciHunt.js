eb91bb2d8cef2ebf2ebee6c13cd40d1bdaa2be60
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 04:47:52 +0200

    new file:   FibonacciHunt.js

---
+++ FibonacciHunt.js
@@ -0,0 +1,17 @@
+function nthFibonacci(n) {
+  if (!Number.isInteger(n)) return undefined;
+
+  if (n === 0) {
+    return n;
+  }
+
+  if (n < 2) {
+    return 1;
+  }
+
+  return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+}
+
+module.exports = {
+  nthFibonacci,
+};
