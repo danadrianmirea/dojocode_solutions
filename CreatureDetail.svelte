1a97073fb360ce338c30b6bc7c8044495fc9b621
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 03:48:37 +0200

    new file:   CreatureDetail.svelte

---
+++ CreatureDetail.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let selectedCreature;
+</script>
+
+{#if selectedCreature}
+  <div class="detail">
+    <h2>{selectedCreature.name}</h2>
+    <p>{selectedCreature.description}</p>
+  </div>
+{/if}
+
+<style>
+  .detail {
+    margin-top: 20px;
+    text-align: center;
+  }
+
+  h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+  }
+
+  p {
+    font-size: 18px;
+    color: #555;
+  }
+</style>
