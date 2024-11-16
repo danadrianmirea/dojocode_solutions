43672236c94e2339685eab1bc4173f770da57f39
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 04:26:52 +0200

    new file:   ExploreTreeLevelOrder.js

---
+++ ExploreTreeLevelOrder.js
@@ -0,0 +1,20 @@
+function levelOrderTraversal(root) {
+  if (!root) return [];
+
+  const result = [];
+  const queue = [root];
+
+  while (queue.length > 0) {
+    const node = queue.shift();
+    result.push(node.val);
+
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+
+  return result;
+}
+
+module.exports = {
+  levelOrderTraversal,
+};
