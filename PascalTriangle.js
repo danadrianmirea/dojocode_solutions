98f19d55abd04f8f670c537d4dc9af06aaf772f7
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 05:04:22 +0200

    new file:   PascalTriangle.js

---
+++ PascalTriangle.js
@@ -0,0 +1,23 @@
+function generatePascalsTriangle(numRows) {
+  const triangle = [];
+
+  for (let row = 0; row < numRows; row++) {
+    const newRow = [];
+
+    for (let col = 0; col <= row; col++) {
+      if (col === 0 || col === row) {
+        newRow.push(1);
+      } else {
+        newRow.push(triangle[row - 1][col - 1] + triangle[row - 1][col]);
+      }
+    }
+
+    triangle.push(newRow);
+  }
+
+  return triangle;
+}
+
+module.exports = {
+  generatePascalsTriangle,
+};
