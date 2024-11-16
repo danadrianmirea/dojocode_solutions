78bc455bffe56a525f9db7a3de134839f0157aab
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 03:38:33 +0200

    new file:   TaskItem.vue

---
+++ TaskItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="task">
+    <h3>{{ task }}</h3>
+    <span @click="deleteTask" class="delete">X</span>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps, emit } from "vue";
+
+  const props = defineProps({
+    task: String,
+  });
+
+  const deleteTask = () => {
+    emit("delete-task", props.task);
+  };
+</script>
