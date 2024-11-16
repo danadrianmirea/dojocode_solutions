484eb2d4036384945d42767b40921dfed6ed385a
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 03:40:23 +0200

    new file:   ReadMoreReadLess.vue

---
+++ ReadMoreReadLess.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>
+      Hello, my name is
+      <strong>...</strong>
+      <span v-show="isExpanded">{{ fullText }}</span>
+    </p>
+    <button @click="toggleText">
+      {{ isExpanded ? "Show Less" : "Show More" }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  const isExpanded = ref(false);
+
+  const fullText = " Danut";
+
+  const toggleText = () => {
+    isExpanded.value = !isExpanded.value;
+  };
+</script>
+
+<style scoped>
+  button {
+    margin-top: 10px;
+    padding: 5px 15px;
+    cursor: pointer;
+  }
+</style>
