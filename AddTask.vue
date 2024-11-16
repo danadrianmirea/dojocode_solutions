8490040b11134eef843ec66d998c5164ce12778f
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 03:37:54 +0200

    new file:   AddTask.vue

---
+++ AddTask.vue
@@ -0,0 +1,25 @@
+<template>
+  <form class="add-form" @submit.prevent="addTask">
+    <div>
+      <input
+        v-model="taskName"
+        name="text"
+        placeholder="Add task"
+        type="text" />
+    </div>
+    <button type="submit">Save Task</button>
+  </form>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  const taskName = ref("");
+
+  const addTask = () => {
+    if (taskName.value.trim()) {
+      emit("add-task", taskName.value);
+      taskName.value = "";
+    }
+  };
+</script>
