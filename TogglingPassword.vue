ac1555c05931ec7a3ede3edb7b9c2be46d1ceb8f
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 03:40:56 +0200

    new file:   TogglingPassword.vue

---
+++ TogglingPassword.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input
+      :type="isVisible ? 'text' : 'password'"
+      v-model="password"
+      placeholder="Enter password" />
+    <button @click="toggleVisibility" class="toggle-btn">
+      {{ isVisible ? "Hide" : "Show" }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  const isVisible = ref(false);
+
+  const password = ref("");
+
+  const toggleVisibility = () => {
+    isVisible.value = !isVisible.value;
+  };
+</script>
+
+<style scoped>
+  .toggle-btn {
+    margin-left: 10px;
+    padding: 5px 10px;
+    cursor: pointer;
+  }
+</style>
