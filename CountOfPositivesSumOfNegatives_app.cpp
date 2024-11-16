1a81dbe550fc7eae5e6753f635ef38c5ee7d4ef7
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 04:57:15 +0200

    new file:   CountOfPositivesSumOfNegatives_app.cpp

---
+++ CountOfPositivesSumOfNegatives_app.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+std::vector<int> countPositivesSumNegatives(std::vector<int> input)
+{
+    // go for it!
+    if(input.size() == 0) return {};
+
+    int countPos = 0;
+    int sumNeg = 0;
+
+    for(int i=0; i<input.size(); i++)
+    {
+        if(input[i] > 0)
+        {
+            countPos++;
+        }
+        else if(input[i] < 0)
+        {
+            sumNeg += input[i];
+        }
+    }
+
+    return {countPos, sumNeg};
+}
