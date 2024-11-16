0fb692035ccf9c1a12c9d319e63a0f33644b142a
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-16 03:42:20 +0200

    new file:   ColorSwitcher.vue

---
+++ ColorSwitcher.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Color switcher</h1>
+    <p :class="isGreen ? 'green' : ''">Make me green</p>
+    <button @click="toggleColor">Toggle color</button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue";
+
+  const isGreen = ref(false);
+
+  function toggleColor() {
+    isGreen.value = !isGreen.value;
+  }
+</script>
+
+<style>
+  .green {
+    color: green;
+  }
+</style>
